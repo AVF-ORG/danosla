1969ca492c48054e8e9ce576cd9381adc4f993a3
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2026-04-27 18:21:53 +0100

    do some updates

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('12345678'),
             'remember_token' => Str::random(10),
             'country_id' => \App\Models\Country::inRandomOrder()->first()?->id,
             'sector_id' => \App\Models\Sector::inRandomOrder()->first()?->id,
